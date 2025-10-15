da6fe52aa1d774e4fe727fdf7fbd0062cd2dece1
CelinShabboh <157270940+CelinShabboh@users.noreply.github.com>
2025-10-15 16:39:37 +0300

    Update leaves_cubit.dart

--- lib/ui/views/leaves_view/leaves_controller/leaves_cubit.dart
+++ lib/ui/views/leaves_view/leaves_controller/leaves_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:dental_project/core/data/models/leave_model.dart';
 import 'package:dental_project/core/data/repository/leave_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 part 'leaves_state.dart';
 
 class LeavesCubit extends Cubit<LeavesState> {
@@ -8,28 +9,36 @@ class LeavesCubit extends Cubit<LeavesState> {
 
   LeavesCubit(this.repository) : super(LeavesInitial());
 
-Future<void> getLeaves({
-  required int companyId,
-  required int departmentId,
-  required int employeeId,
-  required String token,
-}) async {
-  emit(LeavesLoading());
-  try {
-    final response = await repository.fetchLeaves(
-      companyId: companyId,
-      departmentId: departmentId,
-      employeeId: employeeId,
-      token: token, 
-    );
+  Future<void> getLeaves({
+    required int companyId,
+    required int departmentId,
+    required int employeeId,
+    required String token,
+  }) async {
+    emit(LeavesLoading());
+    try {
+      final response = await repository.fetchLeaves(
+        companyId: companyId,
+        departmentId: departmentId,
+        employeeId: employeeId,
+        token: token,
+      );
 
-    final leaves = response.data?.leaves ?? [];
-    final totalSize = response.data?.totalSize ?? leaves.length;
+      final leaves = response.data?.leaves ?? [];
+      final totalSize = (response.data?.totalSize ?? leaves.length).toInt();
 
-    emit(LeavesLoaded(leaves: leaves, totalLeaves: totalSize));
-  } catch (e) {
-    emit(LeavesError(message: e.toString()));
+      final leaveCount = await repository.fetchLeaveCount(
+        employeeId: employeeId,
+        token: token,
+      );
+
+      emit(LeavesLoaded(
+        leaves: leaves,
+        totalLeaves: totalSize,
+        leaveCount: leaveCount,
+      ));
+    } catch (e) {
+      emit(LeavesError(message: e.toString()));
+    }
   }
-}
-
 }
