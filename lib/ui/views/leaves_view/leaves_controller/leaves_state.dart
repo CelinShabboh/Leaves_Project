0bded5f10ec23adf0993145c5baf408c6782a052
CelinShabboh <157270940+CelinShabboh@users.noreply.github.com>
2025-10-15 16:39:59 +0300

    Update leaves_state.dart

--- lib/ui/views/leaves_view/leaves_controller/leaves_state.dart
+++ lib/ui/views/leaves_view/leaves_controller/leaves_state.dart
@@ -9,12 +9,28 @@ class LeavesLoading extends LeavesState {}
 class LeavesLoaded extends LeavesState {
   final List<LeaveItem> leaves;
   final int totalLeaves;
+  final Map<String, dynamic>? leaveCount;
 
-  LeavesLoaded({required this.leaves, required this.totalLeaves});
+  LeavesLoaded({
+    required this.leaves,
+    required this.totalLeaves,
+    this.leaveCount,
+  });
+
+  LeavesLoaded copyWith({
+    List<LeaveItem>? leaves,
+    int? totalLeaves,
+    Map<String, dynamic>? leaveCount,
+  }) {
+    return LeavesLoaded(
+      leaves: leaves ?? this.leaves,
+      totalLeaves: totalLeaves ?? this.totalLeaves,
+      leaveCount: leaveCount ?? this.leaveCount,
+    );
+  }
 }
 
 class LeavesError extends LeavesState {
   final String message;
-
   LeavesError({required this.message});
 }
